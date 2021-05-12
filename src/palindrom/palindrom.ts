3851bb0d9bea699886d17edc898aa0992acc4a83
Novanda Ahsan <novandaahsan1@gmail.com>
2021-05-12 07:09:18 +0700

    palindrom - add manual way, make i t better

--- src/palindrom/palindrom.ts
+++ src/palindrom/palindrom.ts
@@ -1,12 +1,35 @@
-const palindrom = (text, feedback = false) => {
-  const textReverse = text.split("").reverse().join("");
+/**
+ * @author Novanda Ahsan
+ * @param text string that need to is-palindrom
+ * @param feedback boolean - return boolean if true - default false
+ * @returns feedback | boolean
+ */
+const palindrom = (text: string, feedback: boolean = false) => {
+  /** fast way */
+  // const textReverse = text
+  //   .split("") // make it to array
+  //   .reverse() // reverse array
+  //   .join(""); // join array back to string;
 
+  /** manual way  */
+  const textArray = text.split("");
+  let reversedTextArray = [];
+
+  for (let i = textArray.length; i >= 0; i--) {
+    reversedTextArray.push(textArray[i - 1]);
+  }
+
+  const reversedText = reversedTextArray.join("");
+
+  /**
+   * return either use feedback or boolean if feedback true
+   */
   if (feedback)
-    return text === textReverse
+    return text === reversedText
       ? `${text} is Palindrom`
-      : `${text} is not palindrom`;
+      : `${text} is not Palindrom`;
 
-  return text === textReverse ? true : false;
+  return text === reversedText;
 };
 
-export default palindrom
+export default palindrom;
